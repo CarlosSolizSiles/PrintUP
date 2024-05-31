f7f372edb67f5249034089c192083970820906da
AxlitoInArg <axel.albarenque.t1vl@gmail.com>
2024-05-31 05:16:43 -0300

    recuperacion de contraseña :D

--- logeo/process_password_reset.php
+++ logeo/process_password_reset.php
@@ -1,7 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
-    
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -13,35 +12,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Conexión fallida: " . $conn->connect_error);
     }
 
-    $sql = "SELECT * FROM usuarios WHERE Email = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $Email);
-    $stmt->execute();
-    $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        $token = bin2hex(random_bytes(50));
-        $expires = date("U") + 1800;
+    $query = "SELECT * FROM usuarios WHERE Email = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $email);
 
-        $sql = "INSERT INTO password_resets (email, token, expires) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sss", $email, $token, $expires);
-        $stmt->execute();
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $token = bin2hex(random_bytes(50));
+            $expires = date("U") + 1800;
 
-        $resetLink = "http://localhost/printup/logeo/reset_password.php?token=$token"; // Cambia "tu_proyecto" por la carpeta de tu proyecto
-        $subject = "Recuperación de Contraseña - PrintUP";
-        $message = "Haz clic en el siguiente enlace para restablecer tu contraseña: $resetLink";
-        $headers = "From: no-reply@localhost";
+            $sql = "INSERT INTO password_resets (email, token, expires) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sss", $email, $token, $expires);
+            $stmt->execute();
 
-        // Enviar el correo electrónico
-        mail($email, $subject, $message, $headers);
+            $resetLink = "http://localhost/printup/logeo/reset_password.php";
+            $subject = "Recuperación de Contraseña - PrintUP";
+            $message = "Haz clic en el siguiente enlace para restablecer tu contraseña: $resetLink";
+            $headers = "From: printup.t1vl@gmail.com";
+    
+            // Enviar el correo electrónico
+            mail($email, $subject, $message, $headers);
 
-        header("Location: request_password_reset.php?success=Correo enviado. Revisa tu bandeja de entrada.");
-    } else {
-        header("Location: request_password_reset.php?error=Correo electrónico no encontrado.");
+            header("Location: request_password_reset.php?success=Correo enviado. Revisa tu bandeja de entrada.");
+        } else {
+            header("Location: request_password_reset.php?error=Correo electrónico no encontrado.");
+        }
     }
+
 
     $stmt->close();
     $conn->close();
 }
-?>
